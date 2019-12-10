87717b69362f69f47364d10ecba51c4fc36eae2d
akshaymetange <34733500+akshaymetange@users.noreply.github.com>
2019-12-10 20:13:08 +0530

    Create AddCountry.php

---
+++ AddCountry.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<script src="jquery.js"></script>
+</head>
+<body>
+<p>select country adya
+:<input name="city"/>
+<br/>
+<input type="submit" value="add country" name="submit">
+</p>
+</body>
+</html>
+<?php
+
+?>
